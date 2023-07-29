73725ec1fbf5381f9ab9d600c7c146e95c9e7dad
Takuya Kinoshita <kktak02@gmail.com>
2023-07-29 13:59:30 +0900

    vue cli and add vue-google-charts

---
+++ front/src/components/CandleChart.vue
@@ -0,0 +1,30 @@
+<template>
+  <p>This is candle chart</p>
+  <GChart
+    :type="chartType"
+    :data="chartData"
+  ></GChart>
+
+</template>
+
+<script>
+  import { GChart } from 'vue-google-charts'
+
+  export default {
+    components: {
+      GChart
+    },
+    props: {
+      chartType: {
+        type: String,
+        default: '',
+      },
+      chartData: {
+        type: Array,
+        default: () => {
+          return []
+        }
+      }
+    }
+  }
+</script>
---
+++ front/src/views/HomeView.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="container">
+    <candle-chart v-bind:chartType="chartType" v-bind:chartData="chartData"></candle-chart>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import CandleChart from '@/components/CandleChart.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    CandleChart,
+  },
+  data() {
+    return {
+      chartType: 'PieChart',
+      chartData: [
+      ['年', '売上', '費用', '収益'],
+      ['2014', 1000, 400, 200],
+      ['2015', 1170, 460, 250],
+      ['2016', 660, 1120, 300],
+      ['2017', 1030, 540, 350]
+      ],
+    }
+  }
+}
+</script>
