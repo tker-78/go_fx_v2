5261f68d8741ae86b9de238aa2860ad475a58604
Takuya Kinoshita <kktak02@gmail.com>
2023-08-25 05:20:06 +0900

    mod main.go

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"time"
 
 	"example.com/tker-78/fx2/models"
@@ -36,5 +37,25 @@ func main() {
 	/*
 		以降はデバッグ用temporary code
 	*/
+
+	timeTime := time.Date(2022, 10, 01, 00, 00, 00, 00, time.Local)
+	df, err := models.GetCandlesAfterTime(timeTime)
+	if err != nil {
+		log.Println(err)
+	}
+
+	df.AddEma(5)
+	df.AddEma(10)
+	df.AddEma(25)
+
+	for i := 0; i < len(df.Candles); i++ {
+		ema1 := df.Emas[0].Value
+		ema2 := df.Emas[1].Value
+
+		candle := df.Candles[i]
+		if ema1[i] <= ema2[i] && ema1[i+1] > ema2[i+1] {
+			df.Signals.Buy(candle.Time, candle.Mid(), 1000, true)
+		}
+	}
 
 }
