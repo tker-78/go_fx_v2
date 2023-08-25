c10a3dd8f556027630c1e90a7f7c8700f3fe0af5
Takuya Kinoshita <kktak02@gmail.com>
2023-08-26 05:18:56 +0900

    add df debug codes

--- main.go
+++ main.go
@@ -2,16 +2,15 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"time"
 
 	"example.com/tker-78/fx2/models"
 )
 
-func spiner(delay time.Duration) {
+func spiner(delay time.Duration, text string) {
 	for {
 		for _, r := range `-\|/` {
-			fmt.Printf("\r%c", r)
+			fmt.Printf("\r%c %s", r, text)
 			time.Sleep(delay)
 		}
 	}
@@ -28,34 +27,40 @@ func main() {
 	// 	models.CreateCandleWithDuration(key, "1m")
 	// }
 
-	go spiner(100 * time.Millisecond)
+	go spiner(100*time.Millisecond, "loading candle stick data...")
 
 	models.CreateCandleWithDuration("5m", "1m")
 
+	models.CreateCandleWithDuration("30m", "5m")
+	models.CreateCandleWithDuration("1h", "30m")
+	models.CreateCandleWithDuration("4h", "1h")
+	models.CreateCandleWithDuration("24h", "4h")
 	// controllers.StartServer()
 
 	/*
 		以降はデバッグ用temporary code
 	*/
 
-	timeTime := time.Date(2022, 10, 01, 00, 00, 00, 00, time.Local)
-	df, err := models.GetCandlesAfterTime(timeTime)
-	if err != nil {
-		log.Println(err)
-	}
+	// timeTime := time.Date(2022, 12, 30, 16, 00, 00, 00, time.Local)
+	// df, err := models.GetCandlesAfterTime(timeTime)
+	// if err != nil {
+	// 	log.Println(err)
+	// }
 
-	df.AddEma(5)
-	df.AddEma(10)
-	df.AddEma(25)
+	// df.AddEma(5)
+	// df.AddEma(10)
+	// df.AddEma(25)
+	// df.AddSignals()
 
-	for i := 0; i < len(df.Candles); i++ {
-		ema1 := df.Emas[0].Value
-		ema2 := df.Emas[1].Value
+	// ema1 := df.Emas[0].Value
+	// ema2 := df.Emas[1].Value
 
-		candle := df.Candles[i]
-		if ema1[i] <= ema2[i] && ema1[i+1] > ema2[i+1] {
-			df.Signals.Buy(candle.Time, candle.Mid(), 1000, true)
-		}
-	}
+	// for i := 1; i < len(df.Candles); i++ {
+	// 	candle := df.Candles[i-1]
+	// 	if ema1[i-1] <= ema2[i-1] && ema1[i] > ema2[i] {
+	// 		df.Signals.Buy(candle.Time, candle.Mid(), 1000, true)
+	// 	}
+	// }
+	// fmt.Println(df.Signals)
 
 }
